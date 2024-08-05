7e27a79d169997903ede90e7748cbc9c08689238
Davi Marcilio <davimarcilio.js@gmail.com>
2024-08-04 23:15:24 -0300

    🔧Chore: Docker config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
